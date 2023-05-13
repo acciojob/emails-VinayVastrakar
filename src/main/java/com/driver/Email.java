7a531c61144bbedfa295db035986a68aff1d55b6
Vinay Vastrakar <Vinayvastrakar13@gmail.com>
2023-05-13 17:47:13 +0530

    Initial commit

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,5 +1,7 @@
 package com.driver;
 
+import java.util.ArrayList;
+
 public class Email {
 
     private String emailId;
@@ -11,10 +13,12 @@ public class Email {
     }
 
     public String getEmailId() {
+
         return emailId;
     }
 
     public String getPassword() {
+
         return password;
     }
 
@@ -25,5 +29,18 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(oldPassword.equals(password) && isValidPassword(newPassword)){
+            password = newPassword;
+            return ;
+        }
+
+    }
+
+    private boolean isValidPassword(String newPassword) {
+        return password.length()>=8 &&
+                password.matches(".*[A-Z],*") &&
+                password.matches(".*[a-z],*") &&
+                password.matches(".*\\d.*") &&
+                password.matches(".*[A-Za-z\\d].*");
     }
 }
